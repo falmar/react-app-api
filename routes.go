377d17fc303c49d9d9cd4b0ee00123ba777ab3d5
falmar <daviddlavier@gmail.com>
2016-08-01 09:00:03 -0400

    ignore test coverage for server.go and routes.go

--- routes.go
+++ routes.go
@@ -1,6 +1,7 @@
 // Copyright 2016 David Lavieri.  All rights reserved.
 // Use of this source code is governed by a MIT License
 // License that can be found in the LICENSE file.
+//+build !test
 
 package main
 
